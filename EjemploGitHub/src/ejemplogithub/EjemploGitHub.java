b81acc3f37a81ee737e0a3cafe17d9e3f5774d44
actas <actas@S10-96649.redes.local>
2018-07-12 11:04:31 -0600

    Cambio v2

--- EjemploGitHub/src/ejemplogithub/EjemploGitHub.java
+++ EjemploGitHub/src/ejemplogithub/EjemploGitHub.java
@@ -16,6 +16,8 @@ public class EjemploGitHub {
      */
     public static void main(String[] args) {
         System.out.println("Guatemala");
+        System.out.println("LOL");
+        System.out.println("Prueba :v");
     }
     
 }

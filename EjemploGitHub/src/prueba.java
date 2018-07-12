882763453e9b526e57a36740da0d564cf6c84f86
javam2018 <javam2018@S10-96636.redes.local>
2018-07-12 11:20:23 -0600

    Cambios version 1.2

---
+++ EjemploGitHub/src/prueba.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author javam2018
+ */
+public class prueba {
+    private String nombre;
+    
+    
+    
+}
